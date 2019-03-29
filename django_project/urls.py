f8d2c9bfa6cd2d9bb47dcf2811055070bc68b57e
PizBernina <r0b1n@mailbox.org>
2019-03-29 06:21:00 -0700

    Background fix

--- django_project/urls.py
+++ django_project/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls'))
-]
+] + static(settings.STATIC_URL)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
